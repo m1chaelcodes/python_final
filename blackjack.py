1550edbc8de26ef96c8866504ff3087f8553c89f
m1chaelcodes <michaellyons@protonmail.com>
2021-05-13 10:36:24 -0700

    Add title, list of cards, and draw random cards

---
+++ blackjack.py
@@ -0,0 +1,7 @@
+import random
+
+cards = ['1','2','3','4','5','6','7','8','9','10','A','J','Q','K']
+
+print("              Blackjack")
+print("-------------------------------------")
+print("Your hand: " + random.choice(cards) + ", " + random.choice(cards))
